5ac975dd0d6c42456f110549463599e10a8f9bf3
Gero Nootz <gero@nootz.us>
2021-12-15 12:55:31 -0600

    update

--- junk.py
+++ junk.py
@@ -2,23 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+frame_i = [1, 2, 3, 4]
+
 fig, ax = plt.subplots()
 xdata, ydata = [], []
 ln, = plt.plot([], [], 'ro')
 
-def init():
-    ax.set_xlim(0, 2*np.pi)
-    ax.set_ylim(-1, 1)
+def init_ani():
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 5)
     return ln,
 
-def update(frame):
-    xdata.append(frame)
-    ydata.append(np.sin(frame))
+def ani(i):
+    xdata.append(i)
+    ydata.append(i)
     ln.set_data(xdata, ydata)
     return ln,
 
-
-
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
-                    init_func=init, blit=True)
+ani = FuncAnimation(fig, ani, frames=frame_i,
+                    init_func=init_ani, blit=True)
 plt.show()
