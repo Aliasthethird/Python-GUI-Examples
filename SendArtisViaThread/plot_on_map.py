bd5d14beba26367ae7a0087520bcce00dad9bffe
Gero Nootz <gero@nootz.us>
2022-01-01 21:50:59 -0600

    add iver animation

--- SendArtisViaThread/plot_on_map.py
+++ SendArtisViaThread/plot_on_map.py
@@ -12,8 +12,9 @@ import threading
 import time
 import logging
 import rasterio
-from rasterio.plot import show
+import rasterio.plot as rioplot
 import random
+import math
 
 # my libs
 import threadartists as ta
@@ -31,46 +32,66 @@ __status__ = 'Prototype'
 
 def rand_coordints():
     scatter_artist = ta.ScatterArtist(q_art, s=50, label='scatter plot')
+    i = 1
     while True:
         new_xy = (-89.63056 + (random.random()-0.5)*0.01, 30.35275 + (random.random()-0.5)*0.01)
         scatter_artist.add_data_to_artist(new_xy)
+        if i%100 == 0:
+            scatter_artist.clear_data()
+        i += 1
         time.sleep(0.1)
 
 def rand_coordints_temp():
-    scatter_artist = ta.ScatterArtist(q_art, s=50, label='scatter plot')
+    scatter_artist = ta.ScatterArtist(q_art, s=50, label='temp scatter plot')
     for i in range(100):
         new_xy = (-89.63056 + (random.random()-0.5)*0.01, 30.35275 + (random.random()-0.5)*0.01)
         scatter_artist.add_data_to_artist(new_xy)
         time.sleep(0.1)
 
-
 def plot_image(): 
-        """Work in progress..."""
-        image = plt.imread('yota.png')    
-        artist = ta.ImageArtist(q_art, label='image plot')
-        artist.add_data_to_artist(image, 0.1, (-89.63099633995162, 30.348934679652352))
-        i = 1
+        artist = ta.ImageArtist(q_art, label='image plot', alpha=1)
+        artist.add_data_to_artist('yota.png', 0.05, (-89.63099633995162, 30.348934679652352))
         while True: 
             new_xy = (-89.63056 + (random.random()-0.5)*0.01, 30.35275 + (random.random()-0.5)*0.01)
-            artist.set_position(new_xy)
+            artist.set_position(new_xy, 0)
+            time.sleep(1)
 
-            if i%10 == 0:
-                artist.clear_data()
-            i += 1
+def animate_iver(): 
+        iver_art = ta.ImageArtist(q_art, label='iver animation', alpha=0.6)
+        trace_art = ta.LineArtist(q_art, label='iver trace', c='r')
+
+        iver_art.add_data_to_artist('Iver.png', 0.05, (iver_art.ax.get_xlim()[0], 30.348934679652352))
+        x_range = iver_art.ax.get_xlim()[1] - iver_art.ax.get_xlim()[0]
+        y_range = iver_art.ax.get_ylim()[1] - iver_art.ax.get_ylim()[0]
+        i = 0
+        while True: 
+            x_pos = iver_art.ax.get_xlim()[0] + i * x_range
+            y_pos = (iver_art.ax.get_ylim()[0]+(y_range/2)) + 0.9*0.5*y_range*math.sin(
+                2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))
+            new_xy = (x_pos, y_pos)
+            deg = 65 * math.cos(2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))
+            iver_art.set_position(new_xy, deg)
+            trace_art.add_data_to_artist(new_xy)
+            i += 0.01
+            if (x_pos-iver_art.ax.get_xlim()[0]) > x_range:
+                trace_art.clear_data()
+                i = 0
+            time.sleep(0.3)
+
+
+
+def plot_geotif(): 
+        """Work in progress..."""
+        artist = ta.GeoTifArtist(q_art, label='GeoTif plot')
+        artist.add_data_to_artist('Stennis_QW.tif')
+        artist.set_xlim(artist.geotif_xlim[0], artist.geotif_xlim[1])
+        artist.set_ylim(artist.geotif_ylim[0], artist.geotif_ylim[1])
+        while True: 
             time.sleep(2)
 
-def init(ax):
-    with rasterio.open('stennis_QW.tif', driver='GTiff') as data:
-        # dem = data.ReadAsArray()
-        # im = ax.imshow(data)
-        im = show(data, ax=ax)
-        print(data.profile)
-
-    # print(im)    
-    return []
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARNING) # print to console
+    logging.basicConfig(level=logging.INFO) # print to console
     # logging.basicConfig(filename='main.log', encoding='utf-8', level=logging.DEBUG) # append to file
     # logging.basicConfig(filename='example.log', filemode='w', level=logging.INFO) # overide file each run
 
@@ -78,14 +99,8 @@ if __name__ == '__main__':
 
     root = tk.Tk()
     root.wm_title("Update mpl in Tk via queue")
-
-    # fig = plt.Figure(figsize=(9.1, 6))
     fig = plt.Figure()
     ax = fig.add_subplot()
-    # ax.get_xaxis().set_visible(False)
-    # ax.get_yaxis().set_visible(False)
-    # ax.axis('off')
-    # ax.axis('equal')
 
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -97,11 +112,13 @@ if __name__ == '__main__':
     toolbar.update()
     toolbar.pack(side=tk.BOTTOM, fill=tk.X)
  
-    threading.Thread(target=rand_coordints, daemon = True).start() 
-    threading.Thread(target=rand_coordints_temp, daemon = True).start() 
+    # threading.Thread(target=rand_coordints, daemon = True).start() 
+    # threading.Thread(target=rand_coordints_temp, daemon = True).start() 
+    threading.Thread(target=plot_geotif, daemon = True).start() 
     # threading.Thread(target=plot_image, daemon = True).start() 
+    threading.Thread(target=animate_iver, daemon = True).start() 
     
-    anim = animation.FuncAnimation(fig, ta.animate, frames=ta.artist_manager(ax, q_art),
-             init_func=lambda: init(ax), interval=50, blit=True, repeat=True)
+    anim = animation.FuncAnimation(fig, ta.animate, frames=ta.artist_manager(ax, fig, q_art),
+             init_func=ta.init, interval=300, blit=True, repeat=True)
 
     tk.mainloop()
