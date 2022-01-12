d24c2e1038161d4de0ab021fb7ea7b9c27de2ff8
Gero Nootz <gero@nootz.us>
2022-01-11 21:25:38 -0600

    moove thread into class

--- artgallery/plot_on_map_init.py
+++ artgallery/plot_on_map_init.py
@@ -29,65 +29,81 @@ __email__ = 'gero.nootz@usm.edu'
 __status__ = 'example'
 
 
-def animate_iver(): 
-        time.sleep(6)
-        iver_art = ag.ImageArtist(gal, label='iver animation', alpha=1, zorder=4)
-        trace_art = ag.LineArtist(gal, label='iver trace', c='r', alpha=0.6, zorder=3)
-        icon_size = 0.015
-        iver_art.add_data_to_artist('Iver_icon_small.png', icon_size, (0, 0), 0)
-        x_range = iver_art.ax.get_xlim()[1] - iver_art.ax.get_xlim()[0]
-        y_range = iver_art.ax.get_ylim()[1] - iver_art.ax.get_ylim()[0]
-        i = 0
-        while True: 
-            x_pos = iver_art.ax.get_xlim()[0] + i * x_range
-            y_pos = (iver_art.ax.get_ylim()[0]+(y_range/2)) + 0.9*0.5*y_range*math.sin(
-                2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))
-            new_xy = (x_pos, y_pos)
-            deg = 65 * (math.cos(2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))-90)
-            iver_art.set_position(new_xy, deg)
-            trace_art.add_data_to_artist(new_xy)
-            i += 0.01
-            if (x_pos-iver_art.ax.get_xlim()[0]) > x_range:
-                trace_art.clear_data()
-                i = 0
-            time.sleep(0.1)
+class AnimateIver(threading.Thread):
+    def __init__(self):
+       threading.Thread.__init__(self, daemon=True)
 
-def animate_wamv(): 
-        time.sleep(2)
-        iver_art = ag.ImageArtist(gal, label='wam-v animation', alpha=1, zorder=4)
-        trace_art = ag.LineArtist(gal, label='vam-v trace', c='g', alpha=0.6, zorder=3)
-        icon_size = 0.02
-        iver_art.add_data_to_artist('WAM-V_icon_small.png', icon_size, (0, 0), 0)
-        x_range = iver_art.ax.get_xlim()[1] - iver_art.ax.get_xlim()[0]
-        y_range = iver_art.ax.get_ylim()[1] - iver_art.ax.get_ylim()[0]
-        i = 0
-        while True: 
-            x_pos = iver_art.ax.get_xlim()[0] + i * x_range
-            y_pos = (iver_art.ax.get_ylim()[0]+(y_range/2)) + 0.9*0.5*y_range*math.sin(
-                2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))
-            new_xy = (x_pos, y_pos)
-            deg = 65 * (math.cos(2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))-90)
-            iver_art.set_position(new_xy, deg)
-            trace_art.add_data_to_artist(new_xy)
-            i += 0.01
-            if (x_pos-iver_art.ax.get_xlim()[0]) > x_range:
-                trace_art.clear_data()
-                i = 0
-            time.sleep(0.1)
+    def __del__(self):
+        print('del')
 
-def plot_geotif(): 
-        """Work in progress..."""
-        sat = ag.GeoTifArtist(gal, label='Sat plot', zorder=5, alpha=1, add_artist_to_init_func=True)
-        sat.add_data_to_artist('Cat_Island_Low_2.tif')
+    def run(self): 
+            time.sleep(6)
+            iver_art = ag.ImageArtist(gal, label='iver animation', alpha=1, zorder=4)
+            trace_art = ag.LineArtist(gal, label='iver trace', c='r', alpha=0.6, zorder=3)
+            icon_size = 0.015
+            iver_art.add_data_to_artist('Iver_icon_small.png', icon_size, (0, 0), 0)
+            x_range = iver_art.ax.get_xlim()[1] - iver_art.ax.get_xlim()[0]
+            y_range = iver_art.ax.get_ylim()[1] - iver_art.ax.get_ylim()[0]
+            i = 0
+            while True: 
+                x_pos = iver_art.ax.get_xlim()[0] + i * x_range
+                y_pos = (iver_art.ax.get_ylim()[0]+(y_range/2)) + 0.9*0.5*y_range*math.sin(
+                    2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))
+                new_xy = (x_pos, y_pos)
+                deg = 65 * (math.cos(2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))-90)
+                iver_art.set_position(new_xy, deg)
+                trace_art.add_data_to_artist(new_xy)
+                i += 0.001
+                if (x_pos-iver_art.ax.get_xlim()[0]) > x_range:
+                    trace_art.clear_data()
+                    i = 0
+                time.sleep(0.1)
 
-        noaachart = ag.GeoTifArtist(gal, label='Cat Island ENC', zorder=6, alpha=0.6, add_artist_to_init_func=True)
-        noaachart.add_data_to_artist('Cat_Island_ENC.tif')
+class AnimateWamV(threading.Thread):
+    def __init__(self):
+       threading.Thread.__init__(self, daemon=True)
 
-        sat.set_xlim(sat.geotif_xlim[0], sat.geotif_xlim[1])
-        sat.set_ylim(sat.geotif_ylim[0], sat.geotif_ylim[1])
+    def run(self): 
+            time.sleep(2)
+            iver_art = ag.ImageArtist(gal, label='wam-v animation', alpha=1, zorder=4)
+            trace_art = ag.LineArtist(gal, label='vam-v trace', c='g', alpha=0.6, zorder=3)
+            icon_size = 0.02
+            iver_art.add_data_to_artist('WAM-V_icon_small.png', icon_size, (0, 0), 0)
+            x_range = iver_art.ax.get_xlim()[1] - iver_art.ax.get_xlim()[0]
+            y_range = iver_art.ax.get_ylim()[1] - iver_art.ax.get_ylim()[0]
+            i = 0
+            while True: 
+                x_pos = iver_art.ax.get_xlim()[0] + i * x_range
+                y_pos = (iver_art.ax.get_ylim()[0]+(y_range/2)) + 0.9*0.5*y_range*math.sin(
+                    2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))
+                new_xy = (x_pos, y_pos)
+                deg = 65 * (math.cos(2*(math.pi/x_range)*(x_pos-iver_art.ax.get_xlim()[0]))-90)
+                iver_art.set_position(new_xy, deg)
+                trace_art.add_data_to_artist(new_xy)
+                i += 0.01
+                if (x_pos-iver_art.ax.get_xlim()[0]) > x_range:
+                    trace_art.clear_data()
+                    i = 0
+                time.sleep(0.1)
 
-        while True: 
-            time.sleep(10)
+
+class PlotGeotif(threading.Thread):
+    def __init__(self):
+       threading.Thread.__init__(self, daemon=True)
+
+    def run(self): 
+            """Work in progress..."""
+            sat = ag.GeoTifArtist(gal, label='Sat plot', zorder=5, alpha=1, add_artist_to_init_func=True)
+            sat.add_data_to_artist('Cat_Island_Low_2.tif')
+
+            noaachart = ag.GeoTifArtist(gal, label='Cat Island ENC', zorder=6, alpha=0.6, add_artist_to_init_func=True)
+            noaachart.add_data_to_artist('Cat_Island_ENC.tif')
+
+            sat.set_xlim(sat.geotif_xlim[0], sat.geotif_xlim[1])
+            sat.set_ylim(sat.geotif_ylim[0], sat.geotif_ylim[1])
+
+            while True: 
+                time.sleep(10)
 
 def _quit():
     root.quit()     
@@ -132,7 +148,9 @@ if __name__ == '__main__':
     button = tk.Button(master=root, text="Quit", command=_quit)
     button.pack(side=tk.BOTTOM)
 
-
+    plot_geotif = PlotGeotif()
+    iver_ani = AnimateIver()
+    wamv_ani = AnimateWamV()
 
     # Using init function is much faster in terms of updating but slower to rescale. It also is not stable!!!
     anim = animation.FuncAnimation(gal.fig,
@@ -142,12 +160,10 @@ if __name__ == '__main__':
                                     blit=True)
 
     
-
-    threading.Thread(target=plot_geotif, daemon = True).start() 
-    threading.Thread(target=animate_iver, daemon = True).start() 
-    threading.Thread(target=animate_wamv, daemon = True).start() 
+    plot_geotif.start()
+    iver_ani.start()
+    wamv_ani.start()
 
     # threading.Thread(target=holdani, args=(anim,), daemon = True).start()   
-
 
     tk.mainloop()
